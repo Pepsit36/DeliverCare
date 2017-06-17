c2b96ddb0a1ab4985af4a97eedcd5e47e48d92eb
Sebastien Duplessy <sebastien@duplessy.eu>
2017-06-17 13:32:47 +0200

    Debut du site

---
+++ include/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>DeliverCare</title>
+		
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	</head>
+	
+	<body>
+		<div class="container">
+			<div class="jumbotron">
+				<h1>DeliverCare</h1>
+				<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+			</div>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	require_once 'include/header.php'
+?>
+
+Page d'accueil
+
+<?php
+	require_once 'include/footer.php'
+?>
